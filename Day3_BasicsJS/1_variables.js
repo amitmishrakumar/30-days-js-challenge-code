6481df0231e20aecfa2d16e993deafa8f109a2ac
amitmishrakumar <amitmishrag2020@gmail.com>
2025-11-22 23:37:59 +0530

    1_variable_js

---
+++ Day3_BasicsJS/1_variables.js
@@ -0,0 +1,24 @@
+// let const var
+
+
+const name = "amit"
+name = "mishra "
+console.log(name);
+
+
+let name = "amit";
+{
+    let name = "mishra ";
+    console.log(name);
+    
+}
+console.log(name);
+
+
+// table 
+let name = "amit ";
+let age = 23
+let sub = "os"
+location = "up"
+// console.log(name, age, sub, location);
+console.table([name ,age,sub,location])
